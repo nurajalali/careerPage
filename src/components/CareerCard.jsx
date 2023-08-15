b39eb6079c35356db0bf72db8ae5c1ae8220f9bd
Nura Jalali <jalalinura@gmail.com>
2023-08-15 14:53:15 +0330

    responsive careerCard done

--- src/components/CareerCard.jsx
+++ src/components/CareerCard.jsx
@@ -1,13 +1,14 @@
 import { ExpressiveCard } from "@carbon/ibm-products/lib/components";
 import { ArrowRight } from "@carbon/react/icons";
-import { Grid, Column } from "@carbon/react";
+import { Grid, Column, Button } from "@carbon/react";
 
 const CareerCard = ({ jobItem }) => {
   return (
     <Grid fullWidth>
       {jobItem.map((job) => (
-        <Column key={job.id} lg={4} md={4} sm={4}>
+        <Column key={job.id} lg={4} md={3} sm={3}>
           <ExpressiveCard
+            className="button--career-card"
             title={job.Title}
             description={job.City}
             primaryButtonText={job.Condition}
